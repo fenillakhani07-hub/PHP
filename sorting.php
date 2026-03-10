350c03cff3f67c4fbe8aae3b4a97e4fe7c70f09a
fenil lakhani <fenillakhani07@gmail.com>
2026-03-10 15:01:20 +0530

    Add files via upload

---
+++ sorting.php
@@ -0,0 +1,19 @@
+<?php
+$a=array(11,2,33,1,40);
+echo"<br> before sorting:<br>";
+print-r($a);
+for($i=0;$i<5;$i++)
+{
+    for($j=$i+1;$j<5;$j++);
+    {
+        if($a[$i]>$a[$])
+            {
+                $c=$a[$i];
+                $a[$i]=$a[$i];
+                $a[$j]=$c;
+            }
+    }
+}
+echo"<br>after sorting:<br>";
+print-r($a);
+?>
